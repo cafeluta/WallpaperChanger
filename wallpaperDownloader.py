299f86e3df42021568553092195153ac29eed238
cafeluta <sebi.ionita23@gmail.com>
2025-09-24 17:18:56 +0300

    A random photo now is downloaded from UNSPLASH with the right file name
    Signed-off-by: cafeluta <sebi.ionita23@gmail.com>

--- wallpaperDownloader.py
+++ wallpaperDownloader.py
@@ -4,31 +4,33 @@ import random
 from pathlib import Path
 from secretAPI import id_key
 
+# number of wallpapers inside the folder
 maxNumberOfWallpapers = 1000
+currentWallpapers = 0
 
 # UNSPLASH API
 # id_key = your UNSPLASH API KEY
-# unsplash_url = "https://api.unsplash.com/search/photos"
+unsplash_url = "https://api.unsplash.com/search/photos"
 
-# # GET request
-# payload = {'query' : 'mountains forest 1920x1080p', 'client_id' : id_key}
-# r = requests.get(unsplash_url, params=payload)
-# r_dict = r.json()
+# GET request
+payload = {'query' : 'mountains desktop wallpaper', 'client_id' : id_key}
+r = requests.get(unsplash_url, params=payload)
+r_dict = r.json()
 
-# print(f"!! INITIAL GET REQUEST: {r.status_code} !!")
+print(f"!! INITIAL GET REQUEST: {r.status_code} !!")
 
-# # Chosing a random photo
-# numOfResults = len(r_dict['results'])
-# chosenResult = random.randint(0, numOfResults - 1)
-# print(f"!! CHOSEN RESULT: {chosenResult} !!")
+# Chosing a random photo
+numOfResults = len(r_dict['results'])
+chosenResult = random.randint(0, numOfResults - 1)
+print(f"!! CHOSEN RESULT: {chosenResult} !!")
 
-# # Getting image url
-# imageUrl = r_dict['results'][chosenResult]['urls']['full']
+# Getting image url
+imageUrl = r_dict['results'][chosenResult]['urls']['full']
 
-# print(f"!! CHOSEN PHOTO WITH URL: {imageUrl} !!")
+print(f"!! CHOSEN PHOTO WITH URL: {imageUrl} !!")
 
-# # Image response
-# r_image = requests.get(imageUrl)
+# Image response
+r_image = requests.get(imageUrl)
 
 # Creating the .png
 currentFolder = Path('./Wallpapers')
@@ -37,8 +39,6 @@ currentFolder = Path('./Wallpapers')
 currentFolder.mkdir(parents=True, exist_ok=True)
 
 try:
-    currentWallpapers = 0
-
     for item in currentFolder.iterdir():
         # saving in separate variables the name and extension of the file
         itemName = item.name
@@ -48,11 +48,18 @@ try:
         if itemExtension == 'png':
             currentWallpapers += 1
 
-
-    wallpaperFileName = "Wallpaper" + str(currentWallpapers).zfill(3)
-    print(wallpaperFileName)
-
+    wallpaperFileName = "Wallpaper" + str(currentWallpapers).zfill(3) + ".png"
 
 except FileNotFoundError:
     print("== Something went WRONG with the app folder! ==")
+
+# don't download more photos than maxNumberOfPhotos
+if currentWallpapers >= maxNumberOfWallpapers:
+    print("!! Max number of wallpapers reached !!")
+else:
+# Downloading the wallpaper from UNSPLASH
+    wallpaperLocation = currentFolder / wallpaperFileName
+    with open(wallpaperLocation, "wb") as f:
+        f.write(r_image.content)
+        print(f"!! Finished downloading photo with name: {wallpaperFileName} !!")
 
