87056af9a8a76dfaf282c55f236119fa2fa3fe39
cafeluta <sebi.ionita23@gmail.com>
2025-09-24 17:25:53 +0300

    Added a script that changes the wallpaper in win11
    Signed-off-by: cafeluta <sebi.ionita23@gmail.com>

---
+++ wallpaperChanger.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import random
+import ctypes
+import subprocess
+
+# wallpaper folder
+folder = Path("./Wallpapers")
+wallpapers = list(folder.glob("*.png"))
+
+# random wallpaper from the list
+chosen_wallpaper = random.choice(wallpapers)
+
+# Set the walppaer in win11
+ctypes.windll.user32.SystemParametersInfoW(20, 0, str(chosen_wallpaper.resolve()), 3)
+
+# Run the script so it downloads a new photo
+subprocess.run(["python", "download_wallpapers.py"])
